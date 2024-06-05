d79098460daafaf99aefbb2a4b335cf3849c7a40
julien.bouin.etu <julien.bouin.etu@univ-lille.fr>
2024-06-05 19:30:23 +0100

    ajout bouton logout

--- src/app/(tabs)/menu2/_layout.tsx
+++ src/app/(tabs)/menu2/_layout.tsx
@@ -1,7 +1,20 @@
-import { Stack } from "expo-router";
+import { Stack, router } from "expo-router";
+import { Pressable, Text } from "react-native";
+
+const handleLogout = () => {
+    // Implémentez ici la logique de déconnexion, par exemple, effacer les informations d'identification, puis rediriger vers l'écran d'authentification
+    router.push("(auth)")
+};
 
 export default function MenuStack() {
-    return (<Stack>
+    return (<Stack screenOptions={{
+        headerTitleAlign: 'center',
+        headerRight: () => (
+            <Pressable onPress={handleLogout}>
+                <Text style={{ color: 'black', marginRight: 10 }}>Logout</Text>
+            </Pressable>
+        ),
+    }}>
         <Stack.Screen name="index" options={{ title: 'Menu' }} />
     </Stack >
     );
